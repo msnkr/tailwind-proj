8451caccf1fb60392da84d44dc41b36bb776bede
msnkr <mikyle.designs@gmail.com>
2024-09-20 11:59:57 +0200

    .

--- Base Apparel coming soon page/script.js
+++ Base Apparel coming soon page/script.js
@@ -1,8 +1,23 @@
 emailButton = document.querySelector(".btn");
+inputValue = document.querySelector(".emailInput");
+
 successButton = document.querySelector(".success");
 failButton = document.querySelector(".failure");
 
+function isValidEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  }
+
+function showToast(x) {
+    x.classList.add("opacity-0");
+    x.classList.add("opacity-100");
+}
+
 emailButton.addEventListener("click", () => {
-    successButton.classList.toggle("opacity-0");
-    successButton.classList.toggle("opacity-100");
+    if (isValidEmail(inputValue.value)) {
+        showToast(successButton);
+    } else {
+        showToast(failButton);
+    }
 })
