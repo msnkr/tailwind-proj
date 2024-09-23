f6c6675b9306652dbd6088d603ad5691cf9eeccd
msnkr <mikyle.designs@gmail.com>
2024-09-23 11:22:20 +0200

    .

--- Newsletter sign-up form with success message/script.js
+++ Newsletter sign-up form with success message/script.js
@@ -11,13 +11,13 @@ function isValidEmail(email) {
 
 
 btn.addEventListener("click", () => {
-    if (isValidEmail(inputText.value)) {
-        errorMessage.classList.remove("opacity-100");
-        inputText.classList.remove("bg-red-100", "text-red-400")
-        inputBorder.classList.remove("bg-red-100", "border-red-100")
-    } else {
-        errorMessage.classList.add("opacity-100");
-        inputText.classList.add("bg-red-100", "text-red-400")
-        inputBorder.classList.add("bg-red-100", "border-red-100")
-    }
-})
+    const toggleClasses = (add) => {
+        errorMessage.classList.toggle("opacity-100", add);
+        inputText.classList.toggle("bg-red-100", add);
+        inputText.classList.toggle("text-red-400", add);
+        inputBorder.classList.toggle("bg-red-100", add);
+        inputBorder.classList.toggle("border-red-100", add);
+    };
+
+    toggleClasses(!isValidEmail(inputText.value));
+});
