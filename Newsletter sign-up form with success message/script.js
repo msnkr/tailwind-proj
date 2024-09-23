19c53d1f719ec5b2e4dec4368395151680fb0e8c
msnkr <mikyle.designs@gmail.com>
2024-09-23 12:44:57 +0200

    .

--- Newsletter sign-up form with success message/script.js
+++ Newsletter sign-up form with success message/script.js
@@ -3,6 +3,11 @@ inputText = document.querySelector(".input");
 inputBorder = document.querySelector(".input-div");
 errorMessage = document.querySelector(".wrong-email");
 
+mainContainer = document.querySelector(".container");
+successContainer = document.querySelector(".success-container");
+userEmail = document.querySelector(".email-name");
+dismissBtn = document.querySelector(".dissmiss-btn");
+
 
 function isValidEmail(email) {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -12,7 +17,9 @@ function isValidEmail(email) {
 
 btn.addEventListener("click", () => {
     if (isValidEmail(inputText.value)) {
-        console.log("pass")
+        mainContainer.classList.toggle("hidden");
+        successContainer.classList.toggle("hidden");
+        userEmail.textContent = inputText.value;
     } else {
         errorMessage.classList.toggle("opacity-100");
         inputBorder.classList.toggle("border-red-100");
@@ -22,3 +29,6 @@ btn.addEventListener("click", () => {
     }
 });
 
+dismissBtn.addEventListener("click", () => {
+    window.location.reload();
+})
