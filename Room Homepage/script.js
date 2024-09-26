5658687c24783aca42e7b6745feea6133dd1558d
msnkr <mikyle.designs@gmail.com>
2024-09-26 13:51:51 +0200

    .

--- Room Homepage/script.js
+++ Room Homepage/script.js
@@ -2,12 +2,12 @@ burgerIcon = document.querySelector(".burger-icon");
 navItems = document.querySelector(".nav-list");
 leftBtn = document.querySelector(".left");
 rightBtn = document.querySelector(".right");
-
-
+imgs = document.querySelectorAll(".carousel-img");
 
 // Functions
 
 // Events
+
 burgerIcon.addEventListener("click", () => {
     navItems.classList.toggle("hidden");
-})
+});
