fa8784393bb9815a31bec87747b52f6c53dbce80
msnkr <mikyle.designs@gmail.com>
2024-09-30 14:01:34 +0200

    .

--- full-screen-background/script.js
+++ full-screen-background/script.js
@@ -1,6 +1,29 @@
-const images = document.querySelectorAll(".image"), 
-rightBtn = document.querySelector(".right"),
-leftBtn = document.querySelector(".left");
+const images = document.querySelectorAll(".image"),
+    rightBtn = document.querySelector(".right"),
+    leftBtn = document.querySelector(".left");
 
 let idx = 0;
 
+rightBtn.addEventListener("click", () => {
+    if (idx < images.length - 1) {
+        images[idx].classList.toggle("hidden");
+        idx++;
+        images[idx].classList.toggle("hidden");
+    } else {
+        images[idx].classList.toggle("hidden")
+        idx = 0;
+        images[idx].classList.toggle("hidden")
+    }
+})
+
+leftBtn.addEventListener("click", () => {
+    if (idx != 0) {
+        images[idx].classList.toggle("hidden");
+        idx--;
+        images[idx].classList.toggle("hidden");
+    } else {
+        images[idx].classList.toggle("hidden");
+        idx = images.length - 1;
+        images[idx].classList.toggle("hidden");
+    }
+})
