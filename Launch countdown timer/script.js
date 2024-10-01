75baf57af11fafa3f30053584fcb121fff5af943
msnkr <mikyle.designs@gmail.com>
2024-10-01 16:11:55 +0200

    .

---
+++ Launch countdown timer/script.js
@@ -0,0 +1,14 @@
+
+
+// Make a function that converts days, hours, minutes, seconds into milliseconds
+function convertToMilli(days, hours, minutes, seconds) {
+    daysInSeconds = days * 24 * 60 * 60;
+    hoursInSeconds = hours * 60 * 60;
+    minutesInSeconds = minutes * 60;
+    return daysInSeconds + hoursInSeconds + minutesInSeconds + seconds;
+}
+
+
+// Make a function that minuses the seconds
+// Make a function that displays the items on the screen in their correct place
+// Make a function that converts it back into days hours minutes and seconds. 
