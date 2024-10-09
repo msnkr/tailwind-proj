2728528b2d14bdaaa5cb691e641b8938e737330d
msnkr <mikyle.designs@gmail.com>
2024-10-09 15:12:28 +0200

    .

--- Bookmark Landing Page/script.js
+++ Bookmark Landing Page/script.js
@@ -4,7 +4,17 @@ const burgerIcon = document.querySelector(".burger-icon");
 const arrows = document.querySelectorAll(".arrow");
 const faqitem = document.querySelectorAll(".faq-item");
 
+function toggleFade(index) {
+    faqitem[index].classList.toggle('animate-fadeIn')
+}
 
+arrows.forEach((element, index) => {
+    element.addEventListener("click", () => {
+        faqitem[index].classList.toggle("hidden");
+        toggleFade(index);
+
+    })
+})
 
 burgerIcon.addEventListener("click", () => {
     navItems.classList.toggle("hidden");
